5cbbede3a74b17da19ea5dd117a33b1ad0f3baed
Daniel Robinson <dan7c2@gmail.com>
2023-04-16 17:17:06 +0100

    add DbTx ext method

--- Common.Server/Session.cs
+++ Common.Server/Session.cs
@@ -1,10 +1,8 @@
-﻿using System.Net;
-using System.Security;
+﻿using System.Security;
 using System.Security.Cryptography;
 using Common.Shared;
 using MessagePack;
 using Microsoft.AspNetCore.Http;
-using Microsoft.Extensions.DependencyInjection;
 
 namespace Common.Server;
 
@@ -214,83 +212,5 @@ internal record SessionManager : ISessionManager
                 SameSite = SameSiteMode.Strict
             }
         );
-    }
-}
-
-public static class HttpContextExts
-{
-    // these require that ISessionManager was added to service container
-    public static T Get<T>(this HttpContext ctx)
-        where T : notnull
-    {
-        return ctx.RequestServices.GetRequiredService<T>();
-    }
-
-    private static ISessionManager GetSessionManager(this HttpContext ctx)
-    {
-        return ctx.Get<ISessionManager>();
-    }
-
-    public static Session GetSession(this HttpContext ctx)
-    {
-        return ctx.GetSessionManager().Get(ctx);
-    }
-
-    public static Session GetAuthedSession(this HttpContext ctx)
-    {
-        var ses = ctx.GetSession();
-        ctx.ErrorIf(ses.IsAnon, S.AuthNotAuthenticated, null, HttpStatusCode.Unauthorized);
-        return ses;
-    }
-
-    public static Session CreateSession(
-        this HttpContext ctx,
-        string userId,
-        bool isAuthed,
-        bool rememberMe,
-        string lang,
-        string dateFmt,
-        string timeFmt
-    )
-    {
-        return ctx.GetSessionManager()
-            .Create(ctx, userId, isAuthed, rememberMe, lang, dateFmt, timeFmt);
-    }
-
-    public static Session ClearSession(this HttpContext ctx)
-    {
-        return ctx.GetSessionManager().Clear(ctx);
-    }
-
-    public static void ErrorIf(
-        this HttpContext ctx,
-        bool condition,
-        string key,
-        object? model = null,
-        HttpStatusCode code = HttpStatusCode.InternalServerError
-    )
-    {
-        Throw.If(condition, () => new RpcException(ctx.String(key, model), (int)code));
-    }
-
-    public static void ErrorFromValidationResult(
-        this HttpContext ctx,
-        ValidationResult res,
-        HttpStatusCode code = HttpStatusCode.InternalServerError
-    )
-    {
-        Throw.If(
-            !res.Valid,
-            () =>
-                new RpcException(
-                    $"{ctx.String(res.Message.Key, res.Message.Model)}{(res.SubMessages.Any() ? $":\n{string.Join("\n", res.SubMessages.Select(x => ctx.String(x.Key, x.Model)))}" : "")}",
-                    (int)code
-                )
-        );
-    }
-
-    public static string String(this HttpContext ctx, string key, object? model = null)
-    {
-        return ctx.Get<S>().GetOrAddress(ctx.GetSession().Lang, key, model);
     }
 }

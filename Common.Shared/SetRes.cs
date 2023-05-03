f0a017a4c1d6895815754d6cc975a8015f1ec132
Daniel Robinson <dan7c2@gmail.com>
2023-05-03 13:46:34 +0100

    fmt

--- Common.Shared/SetRes.cs
+++ Common.Shared/SetRes.cs
@@ -2,5 +2,5 @@ namespace Common.Shared;
 
 // used for returning a paginated set of results with the more flag
 // to indicate if there are any more items in the set beyond what
-// was returned 
+// was returned
 public record SetRes<T>(IReadOnlyList<T> Set, bool More);

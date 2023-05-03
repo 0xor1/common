540b20744b46d823a40eb5f761906bf67b3314bd
Daniel Robinson <dan7c2@gmail.com>
2023-05-03 19:17:06 +0100

    add helper func for setres

--- Common.Shared/SetRes.cs
+++ Common.Shared/SetRes.cs
@@ -3,4 +3,17 @@ namespace Common.Shared;
 // used for returning a paginated set of results with the more flag
 // to indicate if there are any more items in the set beyond what
 // was returned
-public record SetRes<T>(IReadOnlyList<T> Set, bool More);
+public record SetRes<T>(IReadOnlyList<T> Set, bool More)
+{
+    // qryLimit should always be the desired limit plus 1 to
+    // work out if there are any more pages of items to look up.
+    public static SetRes<T> FromLimit(List<T> set, int qryLimit)
+    {
+        if (set.Count == qryLimit)
+        {
+            set.RemoveAt(qryLimit);
+            return new SetRes<T>(set, true);
+        }
+        return new SetRes<T>(set, false);
+    }
+}

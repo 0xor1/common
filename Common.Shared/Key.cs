6f04f870a632ccbb491ddda5ce87c709f8d2ed58
Daniel Robinson <dan7c2@gmail.com>
2024-05-15 22:08:52 +0100

    what am I doing

--- Common.Shared/Key.cs
+++ Common.Shared/Key.cs
@@ -4,7 +4,7 @@ using System.Text.RegularExpressions;
 namespace Common.Shared;
 
 [TypeConverter(typeof(KeyConverter))]
-public partial record Key : IConvertible
+public partial record Key
 {
     public const int Min = 1;
     public const int Max = 50;
@@ -97,90 +97,5 @@ public partial record Key : IConvertible
     public override string ToString()
     {
         return Value;
-    }
-
-    public TypeCode GetTypeCode()
-    {
-        return TypeCode.String;
-    }
-
-    public bool ToBoolean(IFormatProvider? provider)
-    {
-        throw new InvalidOperationException("can't convert key to bool");
-    }
-
-    public byte ToByte(IFormatProvider? provider)
-    {
-        throw new InvalidOperationException("can't convert key to byte");
-    }
-
-    public char ToChar(IFormatProvider? provider)
-    {
-        throw new InvalidOperationException("can't convert key to char");
-    }
-
-    public DateTime ToDateTime(IFormatProvider? provider)
-    {
-        throw new InvalidOperationException("can't convert key to datetime");
-    }
-
-    public decimal ToDecimal(IFormatProvider? provider)
-    {
-        throw new InvalidOperationException("can't convert key to decimal");
-    }
-
-    public double ToDouble(IFormatProvider? provider)
-    {
-        throw new InvalidOperationException("can't convert key to double");
-    }
-
-    public short ToInt16(IFormatProvider? provider)
-    {
-        throw new InvalidOperationException("can't convert key to int16");
-    }
-
-    public int ToInt32(IFormatProvider? provider)
-    {
-        throw new InvalidOperationException("can't convert key to int32");
-    }
-
-    public long ToInt64(IFormatProvider? provider)
-    {
-        throw new InvalidOperationException("can't convert key to int64");
-    }
-
-    public sbyte ToSByte(IFormatProvider? provider)
-    {
-        throw new InvalidOperationException("can't convert key to sbyte");
-    }
-
-    public float ToSingle(IFormatProvider? provider)
-    {
-        throw new InvalidOperationException("can't convert key to float");
-    }
-
-    public string ToString(IFormatProvider? provider)
-    {
-        return Value;
-    }
-
-    public object ToType(Type conversionType, IFormatProvider? provider)
-    {
-        return Value;
-    }
-
-    public ushort ToUInt16(IFormatProvider? provider)
-    {
-        throw new InvalidOperationException("can't convert key to uint16");
-    }
-
-    public uint ToUInt32(IFormatProvider? provider)
-    {
-        throw new InvalidOperationException("can't convert key to uint32");
-    }
-
-    public ulong ToUInt64(IFormatProvider? provider)
-    {
-        throw new InvalidOperationException("can't convert key to uint64");
     }
 }
